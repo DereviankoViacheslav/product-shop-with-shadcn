966e0e6e31a65700a0a31b60d39324109e7e0f46
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2024-04-05 09:41:44 +0300

    init project & add shadcn & prettier

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+    return (
+        <main>
+            <div>Main page</div>
+            <Button>Button</Button>
+        </main>
+    );
+}
